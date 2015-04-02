bc6490dd64aa4f15d0e5ecc2cc47f83448e4c6f2
dokeun_oh <ohdoking@softwareinlfie.com>
2015-04-02 16:55:36 +0900

    git deploy test11

--- src/main/java/com/ohdoking/jenkins/HomeController.java
+++ src/main/java/com/ohdoking/jenkins/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Welcome home ohdoking! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		System.out.println("testJenkins111");
